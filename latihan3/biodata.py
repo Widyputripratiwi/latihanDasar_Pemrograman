880525d6225415c2a294f872cf132c672cd54db5
unknown <widy15179@gmail.com>
2024-03-09 18:20:40 +0700

    upload biodata dan aplikasi menghitung harga

---
+++ latihan3/biodata.py
@@ -0,0 +1,19 @@
+# membuait variable
+nama = "widy"
+umur = 20
+semester = 2
+kelas = "TI23B"
+jurusan = "Teknik informatika"
+quote = "my life my choice"
+married = False
+adalahPerempuan = True
+hobi = ["nonton", "nyanyi"]
+
+
+print("nama saya", nama)
+print("Semester", semester)
+print("Kelas", kelas)
+print("umur saya", umur)
+print("Jurusan", jurusan)
+print(f"aku suka {hobi[0]} dan {hobi[1]}.")
+print("quote", quote)
